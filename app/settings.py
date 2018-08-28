2c1fc77104eb8f3fd1d100045426d67c7b55aca8
Shota Sugo <s.sugo.pg@gmail.com>
2018-08-29 02:07:57 +0900

    feat: allow hosts for django

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rslg*!l!ml+jjrb!bz0w5ij3voa+3%ut+-16km51mkujjihqs3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 
 # Application definition
